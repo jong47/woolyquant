9a5f0504a74d21d339d0f8d8113c8fc851b6e66a
Jonathan Grady Ong <gradyjonathan55@gmail.com>
2023-07-21 15:52:00 -0700

    stock_pair.cc pybind module
    Added pybind11 module for calculating covariance

---
+++ src/stock_pair.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+// sampleSize cannot be negative, otherwise covariance will not work.
+// NOTE: The returnPrice should not exceed the sampleSize since the total volume should be the same
+//
+// returnPriceA
+// returnPriceB
+// averagePriceA
+// averagePriceB
+
+// class stockPair {
+//     private:
+//         u_int16_t sampleSize;
+//         std::vector<double> returnPriceA;
+//         std::vector<double> returnPriceB;
+//         double averagePriceA;
+//         double averagePriceB;
+//     public:
+//         stockPair(){}
+//         stockPair(u_int16_t sampleSize, double returnPriceA[], double returnPriceB[], double averagePriceA, double averagePriceB) {
+//             this->sampleSize = sampleSize;
+
+//             for (int i = 0; i < sampleSize; i++) {
+//                 this->returnPriceA.insert(this->returnPriceA.begin(), returnPriceA[i]);
+                
+//             }
+
+//             this->averagePriceA = averagePriceA;
+//             this->averagePriceB = averagePriceB;
+//         }
+// };
+
+float calculateCovariance(u_int16_t sampleSize, pybind11::array_t<float> returnPricesA, pybind11::array_t<float> returnPricesB, float averagePriceA, float averagePriceB) {
+    // Get buffer info for returnPricesA
+    pybind11::buffer_info bufA = returnPricesA.request();
+    float* ptrA = static_cast<float*>(bufA.ptr);
+
+    // Get buffer info for returnPricesB
+    pybind11::buffer_info bufB = returnPricesB.request();
+    float* ptrB = static_cast<float*>(bufB.ptr);
+
+    float sigma = 0.0f;
+
+    for (int i = 0; i < sampleSize; i++) {
+        // Access A array data
+        float priceA = ptrA[i];
+        // Access B array data 
+        float priceB = ptrB[i];
+
+        sigma += ((priceA - averagePriceA) * (priceB - averagePriceB));
+        // std::cout << "line " << i << " " << ((priceA - averagePriceA) * (priceB - averagePriceB)) << std::endl;
+        // std::cout << priceA << ' ' << averagePriceA << '\n';
+        // std::cout << priceB << ' ' << averagePriceB << '\n';
+        // std::cout << sigma << '\n';
+    }
+
+    // Cleanup 
+    // returnPricesA.request().ptr = nullptr;
+    // returnPricesB.request().ptr = nullptr;
+
+    float covariance = sigma / (sampleSize - 1);
+    sigma = 0.00f;
+    return covariance;
+}
+
+int pie(int a) {
+    return a + 5;
+}
+
+PYBIND11_MODULE(stock_pair, stock_pair) {
+    stock_pair.doc() = "pybind11 plugin to speed up calculations for Covariance, Correlation, and Cointegration.";
+    stock_pair.def("calculateCovariance", &calculateCovariance, "A function which calculates the covariance between two stocks");
+    stock_pair.def("pie", &pie, "");
+}
+
+// int main(void) {
+    
+    
+//     return 0;   
+// }

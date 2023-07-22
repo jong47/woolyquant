997bcead8a6077b9e58ee4d80bd03e8ebeceaef9
Jonathan Grady Ong <gradyjonathan55@gmail.com>
2023-07-21 17:23:58 -0700

    added correlation formula

--- src/smart_pair.py
+++ src/smart_pair.py
@@ -7,16 +7,13 @@ import ctypes
 import numpy
 import stock_pair
 
-ticks = ["DPZ", "AAPL", "GOOG", "AMD", "GME", "SPY", "NFLX", "BA", "WMT", "GS", "XOM", "NKE", "META", "BRK-B", "MSFT"]
-
-
-def read_csv():
+def read_csv(ticks):
     for t in ticks:
-        pdr.get_data_yahoo(t, start="2022-01-01", end="2023-07-17").to_csv('./res/data/' + t + '.csv')
+        pdr.get_data_yahoo(t, start="2021-01-01", end="2023-07-17").to_csv('./res/data/' + t + '.csv')
 
-def calculate_avg(stock_return: dict, avg_price: dict) -> None:
+def calculate_avg(stock_return: dict, avg_price: dict, std_dev: list) -> None:
     path = "./res/data/"
-    adj_close = 0
+    # adj_close = 0
     sample_size = 384
     for file in os.listdir(path):
         full_path = f'{path}/{file}'
@@ -25,37 +22,53 @@ def calculate_avg(stock_return: dict, avg_price: dict) -> None:
         # For loop runs until EOF for ea. file. --- Big-O: O(n)
         # We convert from Dataframe to series for ea. file
         stock_name = f'{file}'.split('.csv')[0]
-        stock_return[stock_name] = []
-        for i, stock in df.iterrows():
-            adj_close += stock.iloc[5] # 5 is for Adj Close
-            stock_return[stock_name].append(round(stock.iloc[5], 2))
+        
+        # Pulls in the adj close price in percentage form from yahoo
+        data = df['Adj Close'].pct_change() * 100
+        std_dev.append(df['Adj Close'].pct_change().std() * 100)
 
-        rounded_val = round((adj_close / sample_size), 2)
-        avg_price[stock_name] = rounded_val
-        adj_close = 0
+        # We then store the value in list form and round to pretty our data
+        stock_return[stock_name] = data.tolist()
+
+        # Then we calculate the mean percentage as part of the covariance
+        avg_price[stock_name] = data.mean()
 
 def main():
     avg_price = {}
     stock_return = {}
-    # covariance = []
+    covariance = {}
+    std_dev = []
+    ticks = ["DPZ", "AAPL", "GOOG", "AMD", "GME", "SPY", "NFLX", "BA", "WMT", "GS", "XOM", "NKE", "META", "BRK-B", "MSFT"]
+
     # CALL THIS IF YOU THIS IS YOUR FIRST TIME RUNNING THIS PROGRAM
-    read_csv()
+    read_csv(ticks)
     # AFTERWARDS COMMENT IT OUT OTHERWISE YOU WILL MAKE UNNECESSARY CALLS
     
-    calculate_avg(stock_return=stock_return, avg_price=avg_price)
-    # print(avg_price)
+    calculate_avg(stock_return=stock_return, avg_price=avg_price, std_dev=std_dev)
     
     # Covariance = Delta(Return ABC - Average ABC) * (Return XYZ - Average XYZ) / (Sample Size) - 1
+    print("-------- COVARIANCE/CORRELATION/ BETWEEN TWO STOCK PAIRS --------")
     for i in range(0, len(avg_price) - 1):
         for j in range(i + 1, len(avg_price)):
+            # stock_pair_name = '{0} & {1}'.format(ticks[i], ticks[j])
             covariance = stock_pair.calculateCovariance(len(stock_return[ticks[0]]), 
-                                        numpy.array(stock_return[ticks[i]]), 
-                                        numpy.array(stock_return[ticks[j]]),
-                                        avg_price[ticks[i]],
-                                        avg_price[ticks[j]])
-        
-            print(ticks[i] + ' & ' + ticks[j] + ': ', covariance)
-    # print(stock_pair.pie(5))
+                                                                         numpy.array(stock_return[ticks[i]]), 
+                                                                         numpy.array(stock_return[ticks[j]]),
+                                                                         avg_price[ticks[i]],
+                                                                         avg_price[ticks[j]])
+            
+            corr = covariance / (std_dev[i] * std_dev[j])
+
+            print(ticks[i] + ' & ' + ticks[j] + ': ', covariance, corr)
+            # print(corr)          
+            # print(covariance)
+            # print(std_a)
+            # print(std_b)
+
+            # '{ticks[i]} & {ticks[j]}'.format(ticks[i], ticks[j])
+
+    # Correlation
+
     # Cointegration
 
 if __name__ == "__main__":

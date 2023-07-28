0aee61ea420735147c85b12b2b75e818dede8c9b
Jonathan Grady Ong <gradyjonathan55@gmail.com>
2023-07-28 02:53:02 -0700

    Added average spread price calculation and saved chatgpt output to json

--- src/smart_pair.py
+++ src/smart_pair.py
@@ -16,7 +16,7 @@ def read_csv(ticks) -> None:
     for t in ticks:
         pdr.get_data_yahoo(t, start="2021-01-01", end="2023-07-24").to_csv('./res/data/stocks/' + t + '.csv')
 
-def get_stock_data(return_percent: dict, mean_price: dict, std_dev: list) -> None:
+def get_stock_data(return_percent: dict, return_price: dict, mean_return_percent: dict, mean_return_price: dict, std_dev: list) -> None:
     path = "./res/data/stocks"
     for file in os.listdir(path):
         full_path = f'{path}/{file}'
@@ -26,59 +26,80 @@ def get_stock_data(return_percent: dict, mean_price: dict, std_dev: list) -> Non
         # We convert from Dataframe to series for ea. file
         stock_name = f'{file}'.split('.csv')[0]
 
-        # Pulls in the adj close price in percentage form from yahoo
-        data = df['Adj Close'].pct_change().dropna() * 100
-        std_dev[stock_name] = df['Adj Close'].pct_change().std() * 100
+        # Pulls in the adj close price in percentage form
+        percent_data = df['Adj Close'].pct_change().dropna() * 100
+
+        # Pulls in the adj close price in normal value form
+        value_data = df['Adj Close'].dropna()
+        
+        # Calculates standard deviation of Adj Close values, removes any NaN values, and rounds to 4 decimal places
+        std_dev[stock_name] = percent_data.std()
 
         # We then store the value in list form and round to pretty our data
-        return_percent[stock_name] = data.tolist()
+        return_percent[stock_name] = percent_data.to_numpy()
+        return_price[stock_name] = value_data.to_numpy()
 
         # Then we calculate the mean percentage as part of the covariance
-        mean_price[stock_name] = data.mean()
+        mean_return_percent[stock_name] = percent_data.mean()
+        mean_return_price[stock_name] = value_data.mean()
 
-def chatGPT_conversation(parameters) -> None:
+def chatGPT_conversation(pair, parameters) -> None:
     with open('./src/stock_prompt.json') as fp:
         prompt = json.load(fp)
     
-
     # prompt['content'] = parameters
     messages = [{ "role": "user", "content": f'{prompt} {parameters}' }]
 
     response = openai.ChatCompletion.create(model=GPT_MODEL, messages=messages)
-    print(response)
+    with open(f'./res/data/pairs/{pair}.json', 'w', encoding='utf-8') as data:
+        json.dump(response, data, ensure_ascii=False, indent=4)
+
+    # print(response)
 
     api_usage = response['usage']
     print("\n\nCURRENT API USAGE\n\n", api_usage)
 
-def main():
+def load_api_key() -> None:
     load_dotenv('./src/keys.env')
     openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def main():
+    load_api_key()
     ticks = ["DPZ", "AAPL", "GOOGL", "GOOG", "BABA", "JNJ", "JPM", "BAC", "TMO", "AVGO", "CVX", "DHR", "V", "COST", "CRM", "DIS", "CSCO", "QCOM", "AMD", "GME", "SPY", "NFLX", "BA", "WMT", "GS", "XOM", "NKE", "META", "BRK-A", "BRK-B", "MSFT", "AMZN", "NVDA", "TSLA"]
-    mean_price = {}
     return_percent = {}
+    return_price = {}
+    mean_return_percent = {}
+    mean_return_price = {}
     covariance = {}
     std_dev = {}
-    divergences = {}
+    spread_series = {}
 
     read_csv(ticks)
-    
-    get_stock_data(return_percent=return_percent, mean_price=mean_price, std_dev=std_dev)
+
+    get_stock_data(return_percent=return_percent, 
+                   return_price=return_price, 
+                   mean_return_percent=mean_return_percent, 
+                   mean_return_price=mean_return_price, 
+                   std_dev=std_dev)
     
     # Covariance = Delta(Return ABC - Average ABC) * (Return XYZ - Average XYZ) / (Sample Size) - 1
-    print("-------- COVARIANCE/CORRELATION/ADF TEST/P-VAL BETWEEN TWO STOCK PAIRS --------")
-    for i in range(0, len(mean_price) - 1):
-        for j in range(i + 1, len(mean_price)):
-            covariance = stock_pair.calculateCovariance(len(return_percent[ticks[0]]), 
-                                                        numpy.array(return_percent[ticks[i]]), 
-                                                        numpy.array(return_percent[ticks[j]]),
-                                                        mean_price[ticks[i]],
-                                                        mean_price[ticks[j]])
+    # print("-------- COVARIANCE/CORRELATION/ADF TEST/P-VAL BETWEEN TWO STOCK PAIRS --------")
+    sample_size = len(return_percent[ticks[0]])
+    for i in range(0, len(mean_return_percent) - 1):
+        for j in range(i + 1, len(mean_return_percent)):
+            covariance = stock_pair.calculateCovariance(sample_size, 
+                                                        return_percent[ticks[i]], 
+                                                        return_percent[ticks[j]],
+                                                        mean_return_percent[ticks[i]],
+                                                        mean_return_percent[ticks[j]])
             
             # Correlation
             corr = covariance / (std_dev[ticks[i]] * std_dev[ticks[j]])
+
+            # Check if the correlation is really high before we decide pairs
             if corr > 0.79:
                 # Cointegration
-                model = OLS(numpy.array(return_percent[ticks[i]]), numpy.array(return_percent[ticks[j]]))
+                model = OLS(return_percent[ticks[i]], return_percent[ticks[j]])
                 results = model.fit()
 
                 residuals = results.resid
@@ -86,14 +107,13 @@ def main():
                 adf_test = adfuller(residuals)
                 p_val = adf_test[1]
 
-                parameters = f'{ticks[i]} {ticks[j]}: {covariance} {corr} {adf_test[0]} {p_val} {regression_slope}'.format(ticks[i], ticks[j], covariance, corr, adf_test[0], p_val, regression_slope)
-                chatGPT_conversation(parameters=parameters)
+                pair = f'{ticks[i]}-{ticks[j]}'
+                
+                spread_series[pair] = stock_pair.calculateAverageSpread(sample_size, return_price[ticks[i]], return_price[ticks[j]])
 
-                # if p_val > 0.05:
-                #     print(ticks[i] + ' & ' + ticks[j] + ': ' + 'null hypothesis is true')
-
-    # print(return_percent["SPY"])
-    # print(return_percent["MSFT"])
+                parameters = f'{pair}: {covariance} {corr} {adf_test[0]} {p_val} {regression_slope} {spread_series[pair]} {mean_return_price[ticks[i]]} {mean_return_price[ticks[j]]}'
+                print(parameters)
+                chatGPT_conversation(pair=pair, parameters=parameters)
 
 if __name__ == "__main__":
     yf.pdr_override()
--- src/stock_pair.cc
+++ src/stock_pair.cc
@@ -1,38 +1,52 @@
 #include <iostream>
-#include <vector>
+#include <array>
+#include <cmath>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
-float calculateCovariance(u_int16_t sampleSize, pybind11::array_t<float> returnPricesA, pybind11::array_t<float> returnPricesB, float averagePriceA, float averagePriceB) {
-    // Get buffer info for returnPricesA
-    pybind11::buffer_info bufA = returnPricesA.request();
+float calculateCovariance(u_int16_t sampleSize, pybind11::array_t<float> returnPercentA, pybind11::array_t<float> returnPercentB, float averagePercentA, float averagePercentB) {
+    // Get buffer info for returnPercentA
+    pybind11::buffer_info bufA = returnPercentA.request();
     float* ptrA = static_cast<float*>(bufA.ptr);
 
-    // Get buffer info for returnPricesB
-    pybind11::buffer_info bufB = returnPricesB.request();
+    // Get buffer info for returnPercentB
+    pybind11::buffer_info bufB = returnPercentB.request();
     float* ptrB = static_cast<float*>(bufB.ptr);
 
     float sigma = 0.0f;
 
-    for (int i = 1; i < sampleSize; i++) {
+    for (u_int16_t i = 1; i < sampleSize; i++) {
         // Access A array data
-        float priceA = ptrA[i];
+        float percentA = ptrA[i];
         // Access B array data 
-        float priceB = ptrB[i];
+        float percentB = ptrB[i];
 
-        sigma += ((priceA - averagePriceA) * (priceB - averagePriceB));
+        sigma += ((percentA - averagePercentA) * (percentB - averagePercentB));
     }
 
     // Cleanup 
-    // returnPricesA.request().ptr = nullptr;
-    // returnPricesB.request().ptr = nullptr;
+    // returnPercentA.request().ptr = nullptr;
+    // returnPercentB.request().ptr = nullptr;
+    // float covariance = ;
+    // sigma = 0.00f;
+    return (sigma / (sampleSize - 1));
+}
 
-    float covariance = sigma / (sampleSize - 1);
-    sigma = 0.00f;
-    return covariance;
+float calculateAverageSpread(u_int16_t sampleSize, pybind11::array_t<float> returnPricesA, pybind11::array_t<float> returnPricesB) {
+    auto ptrA = static_cast<const float*>(returnPricesA.data());
+    auto ptrB = static_cast<const float*>(returnPricesB.data());
+
+    float spread = 0.00f;
+    for (int i = 0; i < sampleSize; i++) {
+        spread += std::abs(std::abs(ptrA[i]) - std::abs(ptrB[i]));
+    }
+
+    float averageSpread = spread / static_cast<float>(sampleSize);
+    return averageSpread;
 }
 
 PYBIND11_MODULE(stock_pair, stock_pair) {
     stock_pair.doc() = "pybind11 plugin to speed up calculations for Covariance, Correlation, and Cointegration.";
     stock_pair.def("calculateCovariance", &calculateCovariance, "A function which calculates the covariance between two stocks");
+    stock_pair.def("calculateAverageSpread", &calculateAverageSpread, "Function to calculate the average spread price over a time series between two stocks");
 }

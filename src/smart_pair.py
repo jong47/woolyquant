2ce7768d5ba762d5d00b267abc76ad38105d00f9
Jonathan Grady Ong <gradyjonathan55@gmail.com>
2023-08-07 01:37:34 -0700

    Implemented multiprocessing in stock_pair.cc

--- src/smart_pair.py
+++ src/smart_pair.py
@@ -3,6 +3,7 @@ import os
 import json
 import ast
 from multiprocessing import Pool
+from multiprocessing import cpu_count
 
 # Dependencies
 import pandas as pd
@@ -12,7 +13,7 @@ import numpy
 from pandas_datareader import data as pdr
 from statsmodels.regression.linear_model import OLS
 from statsmodels.tsa.stattools import adfuller
-from dotenv import load_dotenv
+from dotenv import load_dotenv # We will deprecate this once we integrate AWS KMS
 
 # Local Files
 import stock_pair
@@ -62,7 +63,7 @@ class SmartPair:
 
     def read_csv(self, ticks) -> None:
         for t in ticks:
-            pdr.get_data_yahoo(t, start="max").to_csv('./res/data/stocks/' + t + '.csv')
+            pdr.get_data_yahoo(t, start="2019-01-01", end="2023-08-04").to_csv('./res/data/stocks/' + t + '.csv')
     
     def get_stock_data(self) -> None:
         path = "./res/data/stocks"
@@ -93,85 +94,83 @@ class SmartPair:
             self.mean_percent_return[ticker] = percent_data.mean()
             self.mean_price_return[ticker] = value_data.mean()
     
-    # NOTE: This function assumes that the stock pairs being passed in args are highly correlated.
-    # Returns an aligned time series list of stock pairs, where [0] is the left_equity and [1] is the right_equity 
+    # NOTE: Returns an aligned time series list of stock pairs, where [0] is the left_equity and [1] is the right_equity 
     def align_time_series(self, left_equity, right_equity) -> list:
-            curr_start = left_equity.first_valid_index()
-            curr_end = left_equity.last_valid_index()
+        curr_start = left_equity['Date'].iloc[0]
+        curr_end = left_equity['Date'].iloc[-1]
 
-            next_start = right_equity.first_valid_index()
-            next_end = right_equity.last_valid_index()
+        next_start = right_equity['Date'].iloc[0]
+        next_end = right_equity['Date'].iloc[-1]
 
-            start = max(curr_start, next_start)
-            end = min(curr_end, next_end)
+        start = max(curr_start, next_start)
+        end = min(curr_end, next_end)
 
-            return list(left_equity.loc[start:end], right_equity.loc[start:end])
-    
+        return list()
+
+    def generate_covariance(self, sample_size: int, left_tick: str, right_tick: str) -> float:
+        return stock_pair.calculateCovariance(sample_size, self.percent_return[left_tick], self.percent_return[right_tick], self.mean_percent_return[left_tick], self.mean_percent_return[right_tick])
+
 
 
 def main():
+    # NOTE: Eventually we want to pull in stock information from the users portfolio from Robinhood
     ticks = ["DPZ", "AAPL", "GOOGL", "GOOG", "BABA", "JNJ", "JPM", "BAC", "TMO", 
              "AVGO", "CVX", "DHR", "V", "MA", "COST", "CRM", "DIS", "CSCO", "QCOM", "AMD", 
              "GME", "SPY", "NFLX", "BA", "WMT", "GS", "XOM", "NKE", "META", "BRK-A", 
              "BRK-B", "MSFT", "AMZN", "NVDA", "TSLA"]
+    
     sp = SmartPair()
-    sp.load_api_key()
 
-
+    # Initialize program
     sp.read_csv(ticks)
-
     sp.get_stock_data()
     
     # Covariance = Delta(Return ABC - Average ABC) * (Return XYZ - Average XYZ) / (Sample Size) - 1
     # print("-------- COVARIANCE/CORRELATION/ADF TEST/P-VAL BETWEEN TWO STOCK PAIRS --------")
 
-    sp.analyze_time_series(ticks)
+    # sp.align_time_series()
 
-    while ticks:
-        left = ticks.pop()
-        right = ticks[0]
+    # Step 1: Align all possible time series.
+    # Step 2: Calculate Covariance
+    # Step 3: Calculate Correlation between stock pairs
+    # Step 4: Determine if correlation is high
+    # Repeat Step 1 otherwise go to step 5
+    
+    # Step 5: Calculate OLS Regression Model for highly correlated stock pairs
+    sample_size = len(sp.price_return[ticks[0]])
+    for i in range(0, len(ticks) - 1):
+        for j in range(i + 1, len(ticks)):
+            left, right = ticks[i], ticks[j]
+            pair = f'{left}-{right}'
 
-
-    for i in range(0, len(mean_percent_return) - 1):
-        for j in range(i + 1, len(mean_percent_return)):
-            covariance = stock_pair.calculateCovariance(sample_size, 
-                                                        percent_return[ticks[i]], 
-                                                        percent_return[ticks[j]],
-                                                        mean_percent_return[ticks[i]],
-                                                        mean_percent_return[ticks[j]])
+            covariance = sp.generate_covariance(sample_size=sample_size, left_tick=left, right_tick=right)
             
-            # Correlation
-            corr = covariance / (std_dev[ticks[i]] * std_dev[ticks[j]])
+            corr = covariance / (sp.std_dev[left] * sp.std_dev[right])
 
             # Check if the correlation is really high before we decide pairs
             if corr > 0.79:
                 # Cointegration
-                model = OLS(percent_return[ticks[i]], percent_return[ticks[j]])
+                model = OLS(sp.percent_return[left], sp.percent_return[right])
                 results = model.fit()
 
                 residuals = results.resid
                 regression_slope = results.params
                 adf_test = adfuller(residuals)
                 p_val = adf_test[1]
-
-                pair = f'{ticks[i]}-{ticks[j]}'
                 
-                spread_series[pair] = stock_pair.calculateAverageSpread(sample_size, price_return[ticks[i]], price_return[ticks[j]])
+                sp.spread_series[pair] = stock_pair.calculateAverageSpread(sample_size, sp.price_return[left], sp.price_return[right])
 
-                # parameters = f'{pair}: {covariance} {corr} {adf_test[0]} {p_val} {regression_slope} {spread_series[pair]} {mean_price_return[ticks[i]]} {mean_price_return[ticks[j]]}'
-                # print(parameters)
+                parameters = f'{pair}: {covariance} {corr} {adf_test[0]} {p_val} {regression_slope} {sp.spread_series[pair]} {sp.mean_price_return[left]} {sp.mean_price_return[right]}'
+                print(parameters)
                 # chatGPT_conversation(pair=pair, parameters=parameters)
                 
                 # Price
-                numpy.savetxt(f'./res/data/stocks/{ticks[i]}-price-series.csv', price_return[ticks[i]], delimiter=",")
-                numpy.savetxt(f'./res/data/stocks/{ticks[j]}-price-series.csv', price_return[ticks[j]], delimiter=",")
+                numpy.savetxt(f'./res/data/stocks/{left}-price-series.csv', sp.price_return[left], delimiter=",")
+                numpy.savetxt(f'./res/data/stocks/{right}-price-series.csv', sp.price_return[right], delimiter=",")
                 
                 # Percent
-                numpy.savetxt(f'./res/data/stocks/{ticks[i]}-percent-series.csv', percent_return[ticks[i]], delimiter=",")
-                numpy.savetxt(f'./res/data/stocks/{ticks[j]}-percent-series.csv', percent_return[ticks[j]], delimiter=",")
-            
-
-                
+                numpy.savetxt(f'./res/data/stocks/{left}-percent-series.csv', sp.percent_return[left], delimiter=",")
+                numpy.savetxt(f'./res/data/stocks/{right}-percent-series.csv', sp.percent_return[right], delimiter=",")        
 
 if __name__ == "__main__":
     yf.pdr_override()
--- src/stock_pair.cc
+++ src/stock_pair.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <array>
 #include <cmath>
+#include <omp.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
-float calculateCovariance(u_int16_t sampleSize, pybind11::array_t<float> returnPercentA, pybind11::array_t<float> returnPercentB, float averagePercentA, float averagePercentB) {
+float calculateCovariance(u_int32_t sampleSize, pybind11::array_t<float> returnPercentA, pybind11::array_t<float> returnPercentB, float averagePercentA, float averagePercentB) {
     // Get buffer info for returnPercentA
     pybind11::buffer_info bufA = returnPercentA.request();
     float* ptrA = static_cast<float*>(bufA.ptr);
@@ -13,9 +14,11 @@ float calculateCovariance(u_int16_t sampleSize, pybind11::array_t<float> returnP
     pybind11::buffer_info bufB = returnPercentB.request();
     float* ptrB = static_cast<float*>(bufB.ptr);
 
+    // Divides loop iterations across multiple threads for sigma
     float sigma = 0.0f;
-
-    for (u_int16_t i = 1; i < sampleSize; i++) {
+    #pragma omp parallel for reduction(+:sigma)
+    
+    for (u_int32_t i = 1; i < sampleSize; i++) {
         float percentA = ptrA[i];
         float percentB = ptrB[i];
 
@@ -25,12 +28,15 @@ float calculateCovariance(u_int16_t sampleSize, pybind11::array_t<float> returnP
     return (sigma / (sampleSize - 1));
 }
 
-float calculateAverageSpread(u_int16_t sampleSize, pybind11::array_t<float> returnPricesA, pybind11::array_t<float> returnPricesB) {
+float calculateAverageSpread(u_int32_t sampleSize, pybind11::array_t<float> returnPricesA, pybind11::array_t<float> returnPricesB) {
     auto ptrA = static_cast<const float*>(returnPricesA.data());
     auto ptrB = static_cast<const float*>(returnPricesB.data());
 
+    // Divides loop iterations across multiple threads for spread
     float spread = 0.00f;
-    for (int i = 0; i < sampleSize; i++) {
+    #pragma omp parallel for reduction(+:spread)
+
+    for (u_int32_t i = 0; i < sampleSize; i++) {
         spread += std::abs(std::abs(ptrA[i]) - std::abs(ptrB[i]));
     }
 
@@ -38,8 +44,13 @@ float calculateAverageSpread(u_int16_t sampleSize, pybind11::array_t<float> retu
     return averageSpread;
 }
 
+// pybind11::list generateStockPairs(pybind11::array_t<float> leftEquity, pybind11::array_t<float> rightEquity) {
+
+// }
+
 PYBIND11_MODULE(stock_pair, stock_pair) {
     stock_pair.doc() = "pybind11 plugin to speed up calculations for Covariance, Correlation, and Cointegration.";
     stock_pair.def("calculateCovariance", &calculateCovariance, "A function which calculates the covariance between two stocks");
     stock_pair.def("calculateAverageSpread", &calculateAverageSpread, "Function to calculate the average spread price over a time series between two stocks");
+    // stock_pair.def("generateStockPairs", &generateStockPairs, "Takes in ");
 }
